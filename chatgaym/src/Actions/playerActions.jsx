fcdb0590090fd7694e8475065818726347f50a7a
franiczio <michal.fr@nelak.pl>
2019-07-01 11:58:19 +0200

    add show players function

---
+++ chatgaym/src/Actions/playerActions.jsx
@@ -0,0 +1,7 @@
+export const SHOW_ACTIVE_PLAYERS = "showActivePlayers";
+
+export default function updateChat() {
+  return {
+    type: SHOW_ACTIVE_PLAYERS
+  };
+}
--- chatgaym/src/Reducers/playerReducer.jsx
+++ chatgaym/src/Reducers/playerReducer.jsx
@@ -1,3 +1,20 @@
-export default function playerReducer(state = "", action) {
-  return "state";
+import SHOW_ACTIVE_PLAYERS from "../Actions/playerActions";
+
+const defaultPlayers = [
+  { nickName: "Michal", isLogged: false },
+  { nickName: "Pietrek", isLogged: true }
+];
+export default function playerReducer(state = defaultPlayers, action) {
+  switch (action.type) {
+    case SHOW_ACTIVE_PLAYERS: {
+      state.map(player => {
+        if (!player.isLogged) {
+          state.pop(player);
+        }
+      });
+      return state;
+    }
+    default:
+      return state;
+  }
 }
